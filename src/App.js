7f8164debfc8401e35106425ff90cefce2947043
KERVY MANIEGO <kervymaniego@gmail.com>
2023-09-29 17:06:03 +0800

    included client 1

--- src/App.js
+++ src/App.js
@@ -4,20 +4,16 @@ import User1 from './components/User1';
 
 function App() {
 
-  const bankEmployee = {
+  const client1 = {
     name: "John Smith",
-    age: 45,
-    position: "Account Manager",
-    accountNumber: "1234  5678"
+    balance: 50000,
+    accountNumber: "1234 5678"
   }
-
-  const address = "123 4 pamela st.";
 
   return (
     <div className="App">
-      <h1>Hello World!</h1>
       <br />
-      <User1 user={bankEmployee} address={address}></User1>
+      <User1 user={client1}></User1>
     </div>
   );
 }
--- src/components/User1.jsx
+++ src/components/User1.jsx
@@ -1,23 +1,13 @@
 
 function User1(props) {
-    // const person1 = {
-    //     firstname: "Kervy",
-    //     lastname: "Maniego",
-    //     balance: "5000",
-    // }
+
     return (
-        <div className='userinfo'>
-            <h1>{props.user.name}<i class="fa-solid fa-pencil"></i></h1>
-            <h2>{props.user.age}</h2>
-            <h2>{props.user.position}</h2>
+        <fieldset className='userinfo'>
+            <h2>Client's Name:{props.user.name}</h2>
+            <h2>{props.user.balance}</h2>
             <h2>{props.user.accountNumber}</h2>
-            <h2>{props.address}</h2><button type='add' id='addbtn'>Deposit</button> <button type='add' id='addbtn'>Withdraw</button> <button type='add' id='addbtn'>Edit</button>  
-        </div>
-        // <fieldset className="user1">
-        //     <p>Balance: ${person1.balance}</p>
-        //     <p>Client's Name: {person1.firstname} {person1.lastname}</p>
-        //     
-        // </fieldset>
+            <button type='add' id='addbtn'>Deposit</button> <button type='add' id='addbtn'>Withdraw</button> <button type='add' id='addbtn'>Edit</button>  
+        </fieldset>
     )
 }
 
