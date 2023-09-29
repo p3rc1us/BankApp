7f1c6603d34301275528352e515808cb3c983ac0
KERVY MANIEGO <kervymaniego@gmail.com>
2023-09-29 18:00:16 +0800

    added a new client

--- src/App.js
+++ src/App.js
@@ -7,14 +7,22 @@ function App() {
   const client1 = {
     name: "John Smith",
     balance: 50000,
-    accountNumber: "1234 5678"
+    accountNumber: "2594474013447193"
+  }
+
+  const client2 = {
+    name: "Elon Musk",
+    balance: 5000000,
+    accountNumber: "4916654547879717"
   }
 
   return (
     <div className="App">
       <br />
       <User1 user={client1}></User1>
+      <User1 user={client2}></User1>
     </div>
+    
   );
 }
 
