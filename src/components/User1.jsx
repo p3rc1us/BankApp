d93e9d6588831508912db5db9ce8397ac27d2884
KERVY MANIEGO <kervymaniego@gmail.com>
2023-10-01 10:28:50 +0800

    added shadows and bg img

--- src/components/User1.jsx
+++ src/components/User1.jsx
@@ -4,7 +4,7 @@ function User1(props) {
     return (
         <fieldset className='userinfo'>
             <h2>Client's Name:{props.user.name}</h2>
-            <h2>{props.user.balance}</h2>
+            <h2>${props.user.balance}</h2>
             <h2>{props.user.accountNumber}</h2>
             <button type='add' id='addbtn'>Deposit</button> <button type='add' id='addbtn'>Withdraw</button> <button type='add' id='addbtn'>Edit</button>  
         </fieldset>
