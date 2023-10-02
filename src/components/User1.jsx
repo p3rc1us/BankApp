3c645f4dd147a8b5e2fc9904bad45dd40ac6c37b
KERVY MANIEGO <kervymaniego@gmail.com>
2023-10-02 21:16:53 +0800

    Deposit and Withdraw working

--- src/components/User1.jsx
+++ src/components/User1.jsx
@@ -5,45 +5,86 @@ function User1(props) {
 
     const [inputVisible, setInputVisible]  = useState(false)
 
+    const [inputVisibleDeposit, setInputVisibleDeposit] = useState(false)
+
     const [inputValue, setInputValue] = useState('')
 
-    const handelInputChange = (event) => {
+    const [inputDeposit, setInputDeposit] = useState('')
+
+    const inputChangeWithdraw = (event) => {
         setInputValue(event.target.value);
     };
 
-    const handleKeyDown = (event) => {
-        if (event.keyCode === 13) {
-            const amountToWithdraw = parseFloat(inputValue);
-            if(!isNaN(amountToWithdraw)) {
-                const newBalance = props.user.balance - amountToWithdraw;
-                props.user.balance = newBalance
+    const inputChangeDeposit =  (event) => {
+        setInputDeposit(event.target.value);
+    }
 
-                setInputValue('');
-                setInputVisible(false)
+const keydownWithdraw = (event) => {
+            if (event.keyCode === 13) {
+                const amountToWithdraw = parseFloat(inputValue);
+                if (!isNaN(amountToWithdraw)) {
+                    if (amountToWithdraw > props.user.balance) {
+                        alert("You have insufficient balance");
+                    } else {
+                        const newBalance = props.user.balance - amountToWithdraw;
+                        props.user.balance = newBalance;
+        
+                        setInputValue('');
+                        setInputVisible(false);
+                    }
                 }
-            
             }
-            
         }
 
+const keydownDeposit = (event) => {
+    if (event.keyCode === 13){
+        const amountToDeposit = parseFloat(inputDeposit);
+        if (!isNaN(amountToDeposit) && amountToDeposit > 0) {
+            const newBalance = props.user.balance + amountToDeposit;
+            props.user.balance = newBalance;
+
+            setInputDeposit('')
+            setInputVisibleDeposit(false);
+        }
+    }
+}
 
     const handleWithdraw = () => {
         console.log("im withdrawing");
 
-        setInputVisible(!inputVisible)
+        setInputVisible(true)
     }
+    const handleDeposit = () => {
+        console.log("im withdrawing");
+
+        setInputVisibleDeposit(true)
+    }
+
 
     return (
         <fieldset className='userinfo'>
             <h2>Client's Name:{props.user.name}</h2>
             <h2>${props.user.balance}</h2>
             <h2>{props.user.accountNumber}</h2>
-            <button type='button' id='addbtn'>Deposit</button> <button type='button' id='addbtn' onClick={handleWithdraw}>Withdraw</button> <button type='add' id='addbtn'>Edit</button>
+            <button type='button' id='addbtn' onClick={handleDeposit}>Deposit</button> <button type='button' id='addbtn' onClick={handleWithdraw}>Withdraw</button> <button type='add' id='addbtn'>Edit</button>
+{/* deposit */}
+            {inputVisibleDeposit && 
+            (<input 
+            type="number" 
+            value={inputDeposit}
+            onChange={inputChangeDeposit}
+            onKeyDown={keydownDeposit}
+             id="inputAmount" 
+             className={inputVisibleDeposit ? 'visible' : 'hidden'}>
+
+             </input>)}
+
+ {/* withdraw */}
             {inputVisible && (<input 
             type="number" 
             value={inputValue}
-            onChange={handelInputChange}
-            onKeyDown={handleKeyDown}
+            onChange={inputChangeWithdraw}
+            onKeyDown={keydownWithdraw}
              id="inputAmount" 
              className={inputVisible ? 'visible' : 'hidden'}>
 
